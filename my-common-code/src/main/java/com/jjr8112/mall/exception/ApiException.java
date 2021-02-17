a12ad256287b3975d963bc101d59ca319c0fec96
jjr8112 <1908641981@qq.com>
2021-02-17 18:47:29 +0800

    通用类

---
+++ my-common-code/src/main/java/com/jjr8112/mall/exception/ApiException.java
@@ -0,0 +1,31 @@
+package com.jjr8112.mall.exception;
+
+import com.jjr8112.mall.api.IErrorCode;
+
+/**
+ * 自定义api异常
+ */
+public class ApiException extends RuntimeException {
+    private IErrorCode errorCode;
+
+    public ApiException(IErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ApiException(String message) {
+        super(message);
+    }
+
+    public ApiException(Throwable cause) {
+        super(cause);
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public IErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
