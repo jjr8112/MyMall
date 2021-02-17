ad6b73b8cd3fc1b8ea2be9e94375b1a713d9a424
jjr8112 <1908641981@qq.com>
2021-02-17 15:46:43 +0800

    README1.0 + my-mbg-package

---
+++ my-mbg-package/src/main/java/com/jjr8112/mall/model/PmsCommentReplay.java
@@ -0,0 +1,104 @@
+package com.jjr8112.mall.model;
+
+import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
+import java.util.Date;
+
+public class PmsCommentReplay implements Serializable {
+    @ApiModelProperty(value = "产品评价回复表")
+    private Long id;
+
+    @ApiModelProperty(value = "评论id，参照comment的id")
+    private Long commentId;
+
+    @ApiModelProperty(value = "会员昵称")
+    private String memberNickName;
+
+    @ApiModelProperty(value = "会员头像")
+    private String memberIcon;
+
+    @ApiModelProperty(value = "回复内容")
+    private String content;
+
+    @ApiModelProperty(value = "回复时间")
+    private Date createTime;
+
+    @ApiModelProperty(value = "评论人员类型；0->会员；1->管理员")
+    private Integer type;
+
+    private static final long serialVersionUID = 1L;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCommentId() {
+        return commentId;
+    }
+
+    public void setCommentId(Long commentId) {
+        this.commentId = commentId;
+    }
+
+    public String getMemberNickName() {
+        return memberNickName;
+    }
+
+    public void setMemberNickName(String memberNickName) {
+        this.memberNickName = memberNickName;
+    }
+
+    public String getMemberIcon() {
+        return memberIcon;
+    }
+
+    public void setMemberIcon(String memberIcon) {
+        this.memberIcon = memberIcon;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", commentId=").append(commentId);
+        sb.append(", memberNickName=").append(memberNickName);
+        sb.append(", memberIcon=").append(memberIcon);
+        sb.append(", content=").append(content);
+        sb.append(", createTime=").append(createTime);
+        sb.append(", type=").append(type);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
